f499fc16c1c062f283b45510add86ead0d3b98da
Vivek <vs690799@gmail.com>
2025-07-16 15:39:01 +0530

    WIP: update departments page

--- src/routes/dashboard/departments/+page.server.ts
+++ src/routes/dashboard/departments/+page.server.ts
@@ -1,4 +1,14 @@
-import type { Actions } from "@sveltejs/kit"
+import { fetchDepartments } from "$lib/data"
+import type { Actions, PageServerLoad } from "./$types"
+
+
+export const load: PageServerLoad = async () => {
+  return {
+    departments: await fetchDepartments()
+
+  }
+}
+
 
 export const actions = {
 } satisfies Actions
--- src/routes/dashboard/departments/+page.svelte
+++ src/routes/dashboard/departments/+page.svelte
@@ -1,8 +1,22 @@
-<script>
+<script lang="ts">
 	import AddDepartmentButton from '$lib/components/departments/AddDepartmentButton.svelte';
+	import type { PageProps } from './$types';
+	let { data }: PageProps = $props();
+	$effect(() => {
+		console.log(data);
+	});
 </script>
 
+<!-- <AddDepartmentButton /> -->
 <div class="ml-5">
-	<AddDepartmentButton />
-	<h1>this is department page</h1>
+	<h2 class="text-xl">Departments</h2>
+	<div class="rounded-md bg-slate-800 px-4 py-2">
+		<ul>
+			{#each data.departments as department}
+				<li class="mb-1.5 cursor-pointer rounded-md px-2 py-1.5 capitalize hover:bg-slate-600">
+					{department.name}
+				</li>
+			{/each}
+		</ul>
+	</div>
 </div>
