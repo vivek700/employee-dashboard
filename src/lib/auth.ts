8eed3a17cd28b7ae5799f83007bee970e166b029
Vivek <vivek.etc@proton.me>
2025-08-23 13:59:25 +0530

    fix: bugs

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,9 @@
 import { betterAuth } from "better-auth";
 import Database from "better-sqlite3"
-import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from "$env/static/private"
+import { BETTER_AUTH_SECRET, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from "$env/static/private"
 
 export const auth = betterAuth({
+	secret: BETTER_AUTH_SECRET,
 	database: new Database("./sqlite.db"),
 	socialProviders: {
 		google: {
